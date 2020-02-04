767b4573eccb4e257e912bb2f889f15c94b53505
Stéphane Blondon <stephane.blondon@gmail.com>
2020-02-04 14:57:05 +0100

    check parameters length on CLI

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 use std::env;
+use std::process;
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        eprintln!("Invalid parameters");
+        process::exit(1);
+    }
+
     let version_a = &args[1];
     let version_b = &args[2];
-
     compare(version_a.to_string(), version_b.to_string())
 }
 
