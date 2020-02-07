30736d842e0af6e442477e28a8341ddabee73190
Stéphane Blondon <stephane.blondon@gmail.com>
2020-02-07 13:40:56 +0100

    Add comparisons

--- src/main.rs
+++ src/main.rs
@@ -11,17 +11,37 @@ fn main() {
 
     let version_a = &args[1];
     let version_b = &args[2];
-    compare(version_a.to_string(), version_b.to_string())
+    compare(version_a.to_string(), version_b.to_string());
 }
 
-fn compare(version_a: String, version_b: String){
+fn compare(version_a: String, version_b: String)-> i32{
     let v_a: i32 = version_a.parse().unwrap();
     let v_b: i32 = version_b.parse().unwrap();
     if v_a > v_b {
-        println!("A {} > B {}", version_a, version_b);
+        return -1;
     } else if v_a == v_b {
-        println!("A {} == B {}", version_a, version_b);
+        return 0;
     } else {
-        println!("A {} < B {}", version_a, version_b);
+        return 1;
     }
+
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+    #[test]
+    fn test_compare_equal() {
+        assert_eq!(compare("2".to_string(), "2".to_string()), 0);
+    }
+    #[test]
+    fn test_compare_inf() {
+        assert_eq!(compare("3".to_string(), "2".to_string()), -1);
+    }
+    #[test]
+    fn test_compare_sup() {
+        assert_eq!(compare("2".to_string(), "3".to_string()), 1);
+    }
+
 }
