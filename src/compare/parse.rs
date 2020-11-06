33a5ed924a59cde94f4eebd93901655320f8c78e
Stephane Blondon <stephane.blondon@gmail.com>
2020-11-06 21:38:30 +0100

    refacto: use tuple instead of Vector so the length is explicit

--- src/compare/parse.rs
+++ src/compare/parse.rs
@@ -3,29 +3,27 @@ use super::structs::{Version, MainBlock, PrereleaseBlock, BuildBlock};
 pub fn parse_raw_version(raw_version: &str) -> Version{
     let version_without_epoch: &str;
     let mut epoch: Option<u8> = None;
-    let splitted_epoch_and_tail: Vec<&str> = split_str(raw_version, ':');
-    if splitted_epoch_and_tail.len() == 2 {
-        epoch = Some(splitted_epoch_and_tail[0].parse().unwrap());
-        version_without_epoch = splitted_epoch_and_tail[1];
+    let (raw_epoch, tail): (&str, &str) = split_str(raw_version, ':');
+    if tail.len() > 0 {
+        epoch = Some(raw_epoch.parse().unwrap());
+        version_without_epoch = tail;
     } else {
-        version_without_epoch = splitted_epoch_and_tail[0];
+        version_without_epoch = raw_epoch;
     }
 
     let mut prerelease_block: Option<PrereleaseBlock> = None;
     let mut build_block: Option<BuildBlock> = None;
-    let version_and_prerelease: Vec<&str> = split_str(&version_without_epoch, '-');
-    let version_and_build: Vec<&str> = split_str(&version_without_epoch, '+');
+    let (version_p, raw_prerelease): (&str, &str) = split_str(&version_without_epoch, '-');
+    let (version_b, raw_build): (&str, &str) = split_str(&version_without_epoch, '+');
     let main_block: MainBlock;
-    if version_and_prerelease.len() == 2 {
-        main_block = parse_main_block(version_and_prerelease[0]);
-        let raw_prerelease: &str = version_and_prerelease[1];
+    if raw_prerelease.len() > 0 {
+        main_block = parse_main_block(version_p);
         prerelease_block = Some(parse_prerelease(&raw_prerelease));
-    } else if version_and_build.len() == 2 {
-        main_block = parse_main_block(version_and_build[0]);
-        let raw_build: &str = version_and_build[1];
+    } else if raw_build.len() > 0 {
+        main_block = parse_main_block(version_b);
         build_block = Some(parse_build(&raw_build));
     } else {
-        main_block = parse_main_block(version_and_prerelease[0]);
+        main_block = parse_main_block(version_p);
     }
     Version {
         epoch: epoch,
@@ -35,8 +33,13 @@ pub fn parse_raw_version(raw_version: &str) -> Version{
     }
 }
 
-fn split_str(s: &str, delimiter: char) -> Vec<&str> {
-    s.split(delimiter).collect()
+fn split_str(s: &str, delimiter: char) -> (&str, &str) {
+    let splitted: Vec<&str> = s.split(delimiter).collect();
+    if splitted.len() == 2 {
+        (splitted[0], splitted[1])
+    } else {
+        (splitted[0], "")
+    }
 }
 
 fn parse_main_block(raw_main_block: &str) -> MainBlock {
@@ -64,11 +67,11 @@ fn last_char_is_letter(s: &str) -> bool {
 fn parse_prerelease(raw_prerelease: &str) -> PrereleaseBlock {
     let step: String;
     let mut post_number: Option<u8> = None;
-    let splitted_prerelease: Vec<&str> = split_str(raw_prerelease, '.');
-    if splitted_prerelease.len() == 2 {
-        step = splitted_prerelease[0].parse().unwrap();
-        post_number = Some(splitted_prerelease[1].parse().unwrap());
-    } else if raw_prerelease[..2] == "rc".to_string() {
+    let (raw_step, raw_number): (&str, &str) = split_str(raw_prerelease, '.');
+    if raw_number.len() > 0 {
+        step = raw_step.parse().unwrap();
+        post_number = Some(raw_number.parse().unwrap());
+    } else if raw_prerelease.len() > 2 && raw_prerelease[..2] == "rc".to_string() {
         step = "rc".to_string();
         post_number = Some(raw_prerelease[2..].parse().unwrap());
     } else {
